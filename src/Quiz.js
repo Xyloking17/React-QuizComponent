7726b51eb75dfc9452e92cc02d3f21f5e751ee6b
Xyloking17 <masonsickler@gmail.com>
2019-11-22 11:49:48 -0600

    daes

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,6 +16,13 @@ class Quiz extends Component {
   }
 
 
+  showNextQuestion() {
+    this.setState((state) => {
+      return {quiz_position: state.quiz_position + 1}
+    });
+  }
+
+
   render() {
     const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
 
@@ -24,7 +31,10 @@ class Quiz extends Component {
       {isQuizEnd ?
         <QuizEnd />
         :
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+        <QuizQuestion
+          showNextQuestionHandler={this.showNextQuestion.bind(this)}
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        />
       }
       </div>
     )
